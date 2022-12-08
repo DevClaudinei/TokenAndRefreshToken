bb65cfa163141ff6d4397386389298ba478860f1
Claudinei Santos <claudinei.santos@warren.com.br>
2022-12-08 14:58:59 -0300

    feat: controllers

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace example_dotnet_identity.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    [HttpGet]
+    [Route("test-auth")]
+    [Authorize]
+    public IActionResult GetTest()
+    {
+        return Ok("Only authenticated user can consume this endpoint");
+    }
+}
---
+++ API/Controllers/AuthenticatorController.cs
@@ -0,0 +1,44 @@
+﻿using Application.Models.DTOs;
+using DomainServices.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace example_dotnet_identity.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticatorController : ControllerBase
+{
+    private readonly IIdentityService _identityService;
+
+    public AuthenticatorController(IIdentityService identityService)
+    {
+        _identityService = identityService ?? throw new ArgumentNullException(nameof(identityService));
+    }
+
+    [HttpPost]
+    [Route("register")]
+    public async Task<IActionResult> CreateUserAsync(UserRegisterRequest userRegisterRequest)
+    {
+        var resultado = await _identityService.RegisterUser(userRegisterRequest);
+
+        return Created("Id:", resultado);
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> LoginAsync(UserLoginRequest userLoginRequest)
+    {
+        try
+        {
+            var resultado = await _identityService.Login(userLoginRequest);
+            return Ok(resultado);
+        }
+        catch(Exception ex)
+        {
+            return Unauthorized(ex.Message);
+        }
+
+    }
+}
